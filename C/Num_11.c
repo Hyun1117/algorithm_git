007afddfe5f3144c64793793884417e84e380ca9
hhgg0017@gmail.com <hhgg0017@gmail.com>
2023-01-15 21:22:31 +0900

    2023. 01.15

---
+++ C/Num_11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int price) {
+    if(price >= 100000 && price < 300000) {
+        price = (price * 0.95);
+    } else if(price >= 300000 && price < 500000) {
+        price = (price * 0.90);
+    } else if(price >= 500000) {
+        price = (price * 0.80);
+    }
+    int answer = price;
+    return answer;
+}
